72bc20f512065b93fe897f8c9c82ed94cde1ce7d
sergioledevs <sergiusgg01@gmail.com>
2023-04-29 00:24:11 +0100

    issuerUrl change

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -15,7 +15,7 @@ const { auth, requiredScopes } = require("express-oauth2-jwt-bearer");
 // Define the options for the auth() middleware function
 const jwtOptions = {
   audience: "https://finalyearprojectapi.onrender.com",
-  issuerBaseURL: "https://cukfit.netlify.app/",
+  issuerBaseURL: "dev-s016gihn6cxe73pi.eu.auth0.com.",
 };
 
 // Authorization middleware. When used, the Access Token must
