a6496bf3cb9c4dba69a0466d6afeaf00f4076c13
sergioledevs <sergiusgg01@gmail.com>
2023-05-18 21:49:31 +0100

    fix cors policy

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -289,7 +289,7 @@ const authMiddleware = (req, res, next) => {
 router.get("/getDatabase", authMiddleware, controllerIndex.index);
 
 router.get("/getRecipes", (req, res) => {
-  res.set("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.set("Access-Control-Allow-Origin", "https://cukfit.netlify.app");
   getRecipesIndex.getRecipes(req, res);
 });
 
